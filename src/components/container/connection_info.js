29ca29c24ea469cebe15e380f059224b383a9a6b
azharprabudi <azharprabui@gmail.com>
2019-05-20 20:15:21 +0700

    create ui connection info

---
+++ src/components/container/connection_info.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../styles/container.css";
+
+const ConnectionInfo = () => (
+  <div className="connection-info">
+    <p>Trying to connect to server ...</p>
+  </div>
+);
+
+export default ConnectionInfo;
