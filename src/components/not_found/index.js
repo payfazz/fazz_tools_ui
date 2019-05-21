0ba7b695b58aa37a24d269181c5a404b51236cd6
Azhar Prabudi <azharprabui@gmail.com>
2019-05-22 00:37:36 +0700

    create not found component

---
+++ src/components/not_found/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+const NotFound = () => (
+  <Row type="flex" justify="center" align="middle">
+    <Col>
+      <img
+        src={`${process.env.PUBLIC_URL}assets/images/svg/no_data.svg`}
+        width={200}
+        height={200}
+        alt={"no data"}
+      />
+      <h1>No Data Found ...</h1>
+    </Col>
+  </Row>
+);
+
+export default NotFound;
