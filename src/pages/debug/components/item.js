870ad93662b2cde772c9510c9fdcbe64e5a54ac5
azharprabudi <azharprabui@gmail.com>
2019-05-21 21:04:53 +0700

    on going debug item

---
+++ src/pages/debug/components/item.js
@@ -0,0 +1,146 @@
+import React, { PureComponent } from "react";
+import { Row, Col, Divider, Table } from "antd";
+import JSONView from "react-json-view";
+import PropTypes from "prop-types";
+import moment from "moment";
+import LogDisplay from "../../../value/log_display";
+import "../../../styles/debug.css";
+
+class DebugItem extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.colorStyle = {
+      color: props.options.color
+    };
+
+    this._columns = [
+      {
+        key: "index",
+        name: "Index",
+        dataIndex: "index"
+      },
+      {
+        key: "type",
+        name: "Data Type",
+        dataIndex: "type"
+      },
+      {
+        key: "value",
+        name: "Value",
+        dataIndex: "value"
+      }
+    ];
+
+    this._createDataSource();
+  }
+
+  _isArray = data => typeof data === "object" && Array.isArray(data);
+
+  _createDataSource = () => {
+    const exlcludeTypes = ["string", "number", "boolean"];
+    const type = typeof this.props.text;
+    if (exlcludeTypes.indexOf(type) > -1) {
+      this._dataSource = [
+        {
+          index: "-",
+          type,
+          value: this.props.text
+        }
+      ];
+
+      return;
+    }
+
+    if (isArray(this.props.text)) {
+      this._dataSource = [
+        {
+          index: "-",
+          type: "array",
+          value: this.props.text
+        }
+      ];
+
+      return;
+    }
+
+    this._dataSource = [];
+    for (let index in this.props.text) {
+      this._dataSource = this._dataSource.concat([
+        {
+          index,
+          type: (() => {
+            if (isArray(this.props.text[index])) {
+              return "array";
+            }
+
+            return typeof this.props.text[index];
+          })(),
+          value: this.props.text[index]
+        }
+      ]);
+    }
+  };
+
+  _displayAsPlainText = () => {
+    if (typeof this.props.text === "object") {
+      return <JSONView src={this.props.text} />;
+    }
+
+    return <p>{this.props.text}</p>;
+  };
+
+  _displayAsTable = () => {
+    return <Table columns={this._columns} dataSource={data} />;
+  };
+
+  render() {
+    return (
+      <Row>
+        {this.props.options.prefix != "" && (
+          <Col>
+            <p className="debug-p" style={this.colorStyle}>
+              {this.props.options.prefix}
+            </p>
+          </Col>
+        )}
+        {(() => {
+          if (this.props.showAs === LogDisplay.plainText) {
+            return this._displayAsPlainText();
+          }
+
+          return null;
+        })()}
+        {this.props.options.suffix != "" && (
+          <Col>
+            <p className="debug-p" style={this.colorStyle}>
+              {this.props.options.suffix}
+            </p>
+          </Col>
+        )}
+        {this.props.options.showTimestamp && (
+          <Col>
+            <p
+              className="debug-p"
+              style={this.colorStyle}
+            >{`Log Time: ${moment().format("YYYY-MM-DD HH:mm:ss")}`}</p>
+          </Col>
+        )}
+        <Divider />
+      </Row>
+    );
+  }
+}
+
+DebugItem.propTypes = {
+  text: PropTypes.any.isRequired,
+  type: PropTypes.string.isRequired,
+  showAs: PropTypes.string.isRequired,
+  options: PropTypes.shape({
+    color: PropTypes.string,
+    prefix: PropTypes.string,
+    suffix: PropTypes.string,
+    showTimestamp: PropTypes.bool
+  })
+};
+
+export default DebugItem;
--- src/pages/debug/index.js
+++ src/pages/debug/index.js
@@ -1,7 +1,9 @@
 import React, { Fragment, PureComponent } from "react";
 import { Drawer } from "antd";
+import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import FloatBtnDebugger from "./components/float_btn_debugger";
+import DebugItem from "./components/item";
 
 class DebugPage extends PureComponent {
   render() {
@@ -14,7 +16,9 @@ class DebugPage extends PureComponent {
           visible={this.props.visible}
           onClose={this.props.onVisibleChange}
         >
-          <h1>helo</h1>
+          {this.props.logs.map(log => (
+            <DebugItem {...log} />
+          ))}
         </Drawer>
         <FloatBtnDebugger onClick={this.props.onVisibleChange} />
       </Fragment>
@@ -27,4 +31,20 @@ DebugPage.propTypes = {
   onVisibleChange: PropTypes.func.isRequired
 };
 
-export default DebugPage;
+const mapStateToProps = ({ debug: { logs } }) => ({
+  logs: [
+    {
+      text: "kondel",
+      showAs: "plainText",
+      type: "text",
+      options: {
+        color: "red",
+        prefix: "kondel",
+        suffix: "ah",
+        showTimestamp: true
+      }
+    }
+  ]
+});
+
+export default connect(mapStateToProps)(DebugPage);
