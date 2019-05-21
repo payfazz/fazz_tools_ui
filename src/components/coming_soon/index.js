8b06a0c98514e35abdcd9ea095bd7533c2b8a510
Azhar Prabudi <azharprabui@gmail.com>
2019-05-22 01:13:32 +0700

    create coming soon component

---
+++ src/components/coming_soon/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Row, Col } from "antd";
+
+const ComingSoon = () => (
+  <Row type="flex" align="middle" justify="center">
+    <Col>
+      <img
+        src={`${process.env.PUBLIC_URL}assets/images/png/coming_soon.png`}
+        alt="coming soon"
+        width={240}
+        height={240}
+      />
+      <h1>
+        <b>Coming Soon</b>
+      </h1>
+    </Col>
+  </Row>
+);
+
+export default ComingSoon;
