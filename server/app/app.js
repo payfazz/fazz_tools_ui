77ecc6aa28f5a9a0b243cdcef75e039c9a36bee1
Willy <w.praditya@yahoo.com>
2019-05-28 14:28:33 +0700

    Refactor app.js in Server

--- server/app/app.js
+++ server/app/app.js
@@ -4,14 +4,23 @@ const createHTTPServer = require("../server/http");
 const createWebSocketServer = require("../server/ws");
 
 const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const { app, BrowserWindow } = electron;
 
 const path = require("path");
 const url = require("url");
 const isDev = require("electron-is-dev");
 
 let mainWindow;
+
+function createServer() {
+  const _server = createHTTPServer({
+    port: config.FAZZ_DEBUGGER_PORT,
+    onServe: () =>
+      console.log(`Server already run at port ${config.FAZZ_DEBUGGER_PORT}`)
+  });
+
+  createWebSocketServer(_server);
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 900, height: 680 });
@@ -21,18 +30,15 @@ function createWindow() {
       : `file://${path.join(__dirname, "../../build/index.html")}`
   );
 
-  const _server = createHTTPServer({
-    port: config.FAZZ_DEBUGGER_PORT,
-    onServe: () =>
-      console.log(`Server already run at port ${config.FAZZ_DEBUGGER_PORT}`)
-  });
-
-  createWebSocketServer(_server);
-
   mainWindow.on("closed", () => (mainWindow = null));
 }
 
-app.on("ready", createWindow);
+function main() {
+  createServer();
+  createWindow();
+}
+
+app.on("ready", main);
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
